c9b3e658043994ba0b1c050d5aa4e4f40a21b881
Ralph Largo <devlargs@gmail.com>
2020-09-27 22:27:24 +0800

    upload file using s3

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+var bodyParser = require("body-parser");
 
 var app = express();
 
@@ -14,6 +14,7 @@ var app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
+app.use(bodyParser.json({ type: "application/*+json" }));
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -21,7 +22,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,56 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const multer = require("multer");
+const multerS3 = require("multer-s3");
+const aws = require("aws-sdk");
+const dayjs = require("dayjs");
+
+const s3 = new aws.S3({
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+});
+
+const upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: process.env.BUCKET_NAME,
+    metadata: function (req, file, cb) {
+      cb(null, { fieldName: file.fieldname });
+    },
+    acl: "public-read",
+    key: function (req, file, cb) {
+      const temp = file.originalname.split(".");
+      const extension = temp[temp.length - 1];
+      cb(null, `${dayjs().format("MM-DD-YYYY-hh:mm")}.${extension}`);
+    },
+  }),
+});
 
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Multer s3 uploader" });
+  res.render("index", { title: "Working prototype" });
+});
+
+router.post("/upload", upload.single("photos"), function (req, res, next) {
+  const { originalname, mimetype, size, key } = req.file;
+
+  s3.deleteObject(
+    {
+      Key: key,
+      Bucket: process.env.BUCKET_NAME,
+    },
+    function (err, data) {
+      if (err) {
+        res.send({
+          error: "Something went wrong",
+        });
+      } else {
+        res.send({
+          size,
+          name: originalname,
+          type: mimetype,
+        });
+      }
+    }
+  );
 });
 
 module.exports = router;
